7698e805da2a810b9fd4c13cd7349f5390e427e9
ckrbqja <gbhermes@naver.com>
2022-03-06 17:09:04 +0900

    first commit

---
+++ mariadb/sqls/initialize.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS sotong;
+
+CREATE DATABASE sotong;
+USE sotong;
+
+-- CREATE TABLE  lists(
+--     id INTEGER AUTO_INCREMENT,
+--     value TEXT,
+--     PRIMARY KEY (id)
+-- )
